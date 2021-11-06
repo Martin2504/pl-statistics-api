805c8c54be824abd7f988cce3a62f7fd09807778
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2021-11-06 14:39:58 +0000

    first commit

---
+++ src/main/java/edu/martin/plstatisticsapi/PlStatisticsApiApplication.java
@@ -0,0 +1,13 @@
+package edu.martin.plstatisticsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlStatisticsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlStatisticsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/martin/plstatisticsapi/PlStatisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.martin.plstatisticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlStatisticsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
